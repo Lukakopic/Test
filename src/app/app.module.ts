5e674cdb826e69da363306544476f16d427aaac0
Luka Kopić <lukakopic14@gmail.com>
2019-02-05 15:45:24 +0100

    add routes and about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
-/*import { HomeComponent } from './app.component';*/
+
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { AboutComponent } from './about/about.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SignUpComponent
+    ContactComponent,
+    SignUpComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule, 
     RouterModule.forRoot([
-      { path: '', component: AppComponent },
-      { path: 'about', component: AppComponent },
-      { path: 'contact', component: AppComponent },
+      //{ path: '', component: AppComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
     ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.js
+++ src/app/navbar/navbar.js
@@ -9,3 +9,21 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
 }
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () { myFunction() };
+
+// Get the navbar
+var topnav = document.getElementById("topnav");
+
+// Get the offset position of the navbar
+var sticky = topnav.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
